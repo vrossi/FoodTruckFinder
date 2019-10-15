4372cccb313704f34df4e7264614671d9dbccd0a
Rossi, Vincent <vrossi@mimeo.com>
2019-10-15 10:25:27 -0400

    Added the main app which contains the UI Added a domain project to do the work of getting the food trucks

---
+++ FoodTruckFinder/Extensions/HelperExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodTruckFinder.Extensions
+{
+    public static class HelperExtensions
+    {
+        public static IEnumerable<TSource> DistinctBy<TSource>(this IEnumerable<TSource> source, params Func<TSource, object>[] keySelectors)
+        {
+            // initialize the table
+            var seenKeysTable = keySelectors.ToDictionary(x => x, x => new HashSet<object>());
+
+            // loop through each element in source
+            foreach (var element in source)
+            {
+                // initialize the flag to true
+                var flag = true;
+
+                // loop through each keySelector a
+                foreach (var (keySelector, hashSet) in seenKeysTable)
+                {
+                    // if all conditions are true
+                    flag = flag && hashSet.Add(keySelector(element));
+                }
+
+                // if no duplicate key was added to table, then yield the list element
+                if (flag)
+                {
+                    yield return element;
+                }
+            }
+        }
+    }
+}
---
+++ FoodTruckFinder/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodTruckFinder.Domain.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NodaTime;
+
+namespace FoodTruckFinder
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddRazorPages();
+
+            services.AddHttpClient<IMobileFoodService, MobileFoodService>(client =>
+            {
+                client.BaseAddress = new Uri("https://data.sfgov.org/");
+            });
+
+            services.AddMemoryCache();
+
+            services.AddSingleton<IClock>(provider => SystemClock.Instance);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
